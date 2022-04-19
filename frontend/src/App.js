0b1714b30f05163958c091889ee27c39665e1470
Parth Sawant <parthsawant97@gmail.com>
2022-04-19 20:04:25 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <h2> Meladie Prediction </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
