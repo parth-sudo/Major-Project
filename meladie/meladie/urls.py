9c4658534ef2350e86e684082d48d4182ac3e549
Parth Sawant <parthsawant97@gmail.com>
2022-08-16 13:40:26 +0530

    removed django.conf.url

--- meladie/meladie/urls.py
+++ meladie/meladie/urls.py
@@ -1,16 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
-from django.conf import settings
-
-from django.conf.urls import url
-from django.views.static import serve 
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('', include('backend.urls')),
     path('auth/', obtain_auth_token),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
